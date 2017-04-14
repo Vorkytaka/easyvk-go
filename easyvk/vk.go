be2b81b2b87246e93efe3f927008e850f9b2d860
SolidlSnake <solidlsnake@yandex.ru>
2017-04-15 01:16:28 +0300

    Base of VK struct and request func

---
+++ easyvk/vk.go
@@ -0,0 +1,45 @@
+package easyvk
+
+import (
+	"net/url"
+	"net/http"
+	"io/ioutil"
+)
+
+const API_URL = "https://api.vk.com/method/"
+
+type VK struct {
+	AccessToken string
+}
+
+func WithToken(token string) VK {
+	vk := VK{token }
+	return vk
+}
+
+func (vk *VK) Request(method string, params map[string]string) ([]byte, error) {
+	u, err := url.Parse(API_URL + method)
+	if err != nil {
+		return nil, err
+	}
+
+	query := url.Values{}
+	for k, v := range params {
+		query.Set(k, v)
+	}
+	query.Set("access_token", vk.AccessToken)
+	u.RawQuery = query.Encode()
+
+	resp, err := http.Get(u.String())
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
