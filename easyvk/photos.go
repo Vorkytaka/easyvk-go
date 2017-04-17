33a1fed59e9396641ea08974c2e9aa727ad5508a
SolidlSnake <solidlsnake@yandex.ru>
2017-04-17 19:46:57 +0300

    Refactor with golint

--- easyvk/photos.go
+++ easyvk/photos.go
@@ -5,12 +5,15 @@ import (
 	"fmt"
 )
 
+// A Photos describes a set of methods
+// to work with photos.
 type Photos struct {
 	vk *VK
 }
 
-func (p *Photos) GetWallUploadServer(group_id uint) (WallUploadServer, error) {
-	params := map[string]string{"group_id": fmt.Sprint(group_id) }
+// GetWallUploadServer returns the server address for photo upload onto a user's wall.
+func (p *Photos) GetWallUploadServer(groupID uint) (WallUploadServer, error) {
+	params := map[string]string{"group_id": fmt.Sprint(groupID) }
 	resp, err := p.vk.Request("photos.getWallUploadServer", params)
 	if err != nil {
 		return WallUploadServer{}, err
