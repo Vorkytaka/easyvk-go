5e815a1fb6382e26f6b3f98df827e68db18d6346
SolidlSnake <solidlsnake@yandex.ru>
2017-04-18 19:06:28 +0300

    Return counts

--- easyvk/fave.go
+++ easyvk/fave.go
@@ -12,41 +12,43 @@ type Fave struct {
 }
 
 // GetUsers returns a list of users whom the current user has bookmarked.
-func (f *Fave) GetUsers(offset, count uint) (FaveUsers, error) {
+// Also this func return count of all bookmarked users.
+func (f *Fave) GetUsers(offset, count uint) (FaveUsers, uint, error) {
 	params := map[string]string{
 		"offset": fmt.Sprint(offset),
 		"count":  fmt.Sprint(count),
 	}
 	resp, err := f.vk.Request("fave.getUsers", params)
 	if err != nil {
-		return FaveUsers{}, err
+		return FaveUsers{}, 0, err
 	}
-	nextResp, count, err := f.vk.ResponseWithCount(resp)
+	nextResp, quantity, err := f.vk.ResponseWithCount(resp)
 	var users FaveUsers
 	err = json.Unmarshal(nextResp, &users)
 	if err != nil {
-		return FaveUsers{}, err
+		return FaveUsers{}, 0, err
 	}
 
-	return users, nil
+	return users, quantity, nil
 }
 
 // GetLinks returns a list of links that the current user has bookmarked.
-func (f *Fave) GetLinks(offset, count uint) (FaveLinks, error) {
+// Also this func return count of all bookmarked links.
+func (f *Fave) GetLinks(offset, count uint) (FaveLinks, uint, error) {
 	params := map[string]string{
 		"offset": fmt.Sprint(offset),
 		"count":  fmt.Sprint(count),
 	}
 	resp, err := f.vk.Request("fave.getLinks", params)
 	if err != nil {
-		return FaveLinks{}, err
+		return FaveLinks{}, 0, err
 	}
-	nextResp, count, err := f.vk.ResponseWithCount(resp)
+	nextResp, quantity, err := f.vk.ResponseWithCount(resp)
 	var links FaveLinks
 	err = json.Unmarshal(nextResp, &links)
 	if err != nil {
-		return FaveLinks{}, err
+		return FaveLinks{}, 0, err
 	}
 
-	return links, nil
+	return links, quantity, nil
 }
