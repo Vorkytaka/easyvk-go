5e528c57b991340d06e1aa72bdec2c19c3b832ca
SolidlSnake <solidlsnake@yandex.ru>
2017-07-30 00:01:27 +0300

    Add links to photos description

--- easyvk/photos.go
+++ easyvk/photos.go
@@ -7,12 +7,14 @@ import (
 
 // A Photos describes a set of methods
 // to work with photos.
+// https://vk.com/dev/photos
 type Photos struct {
 	vk *VK
 }
 
 // PhotosWallUploadServerResponse describes the server address
 // for photo upload onto a user's wall.
+// https://vk.com/dev/photos.getWallUploadServer
 type PhotosWallUploadServerResponse struct {
 	UploadURL string `json:"upload_url"`
 	AlbumID   int `json:"album_id"`
@@ -20,6 +22,7 @@ type PhotosWallUploadServerResponse struct {
 }
 
 // GetWallUploadServer returns the server address for photo upload onto a user's wall.
+// https://vk.com/dev/photos.getWallUploadServer
 func (p *Photos) GetWallUploadServer(groupID uint) (PhotosWallUploadServerResponse, error) {
 	params := map[string]string{"group_id": fmt.Sprint(groupID) }
 	resp, err := p.vk.Request("photos.getWallUploadServer", params)
@@ -36,6 +39,7 @@ func (p *Photos) GetWallUploadServer(groupID uint) (PhotosWallUploadServerRespon
 
 // PhotosSavedWallPhotoResponse describes info about
 // saved photo on wall after being uploaded.
+// https://vk.com/dev/photos.saveWallPhoto
 type PhotosSavedWallPhotoResponse []struct {
 	ID       int `json:"id"`
 	AlbumID  int `json:"album_id"`
@@ -52,6 +56,7 @@ type PhotosSavedWallPhotoResponse []struct {
 
 // SaveWallPhoto saves a photo to a user's or community's wall after being uploaded.
 // For upload look at file upload.go.
+// https://vk.com/dev/photos.saveWallPhoto
 func (p *Photos) SaveWallPhoto(userID, groupID uint, photo, hash, caption string, server int, latitude, longitude float64) (PhotosSavedWallPhotoResponse, error) {
 	params := map[string]string{
 		"user_id":   fmt.Sprint(userID),
