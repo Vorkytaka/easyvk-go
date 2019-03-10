57fb3607575273acd76094fbedecb24907d739b5
smolchanovsky <smolchanovsky@gmail.com>
2019-03-10 15:50:06 +0500

    add friends.get method

---
+++ easyvk/friends.go
@@ -0,0 +1,47 @@
+package easyvk
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+// A Friends describes a set of methods
+// to work with friends.
+// https://vk.com/dev/friends
+type Friends struct {
+	vk *VK
+}
+
+// A FriendsGetResponse describes a list of friends
+// of the user.
+// https://vk.com/dev/friends.get
+type FriendsGetResponse struct {
+	Count int `json:"count"`
+	Items []UserObject
+}
+
+// Get returns a list of friends of the user.
+//https://vk.com/dev/friends.get
+func (f *Friends) Get(userId, fields []string) (FaveGetUsersResponse, error) {
+	var fieldsWithId []string
+	if len(fields) == 0 {
+		fieldsWithId = append(fields, "id")
+	}
+
+	params := map[string]string{
+		"user_id": fmt.Sprint(userId),
+		"fields":  strings.Join(fieldsWithId,","),
+	}
+	resp, err := f.vk.Request("friends.get", params)
+	if err != nil {
+		return FaveGetUsersResponse{}, err
+	}
+	var users FaveGetUsersResponse
+	err = json.Unmarshal(resp, &users)
+	if err != nil {
+		return FaveGetUsersResponse{}, err
+	}
+
+	return users, nil
+}
